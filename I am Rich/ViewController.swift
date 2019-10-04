225c52530d250d99d9603473e02843c12bbd08b8
Jarrett Kong <jarrettkong@me.com>
2019-10-04 12:21:39 -0600

    Initial Commit

---
+++ I am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Jarrett Kong on 10/4/19.
+//  Copyright © 2019 Jarrett Kong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
